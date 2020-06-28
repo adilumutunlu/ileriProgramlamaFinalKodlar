2614d7b8b49c201f6dadbff90e1bbb6eb200fd93
adilumutunlu <67440025+adilumutunlu@users.noreply.github.com>
2020-06-28 15:27:27 +0300

    Add files via upload

---
+++ eular4.js
@@ -0,0 +1,33 @@
+function enBuyukPalindromik(basamak) {
+    var i, n, m, d, inf, sup, limit, number = 0;
+    for (i = 1; i < basamak; i += 1) {
+        number = 10 * number + 9;
+    }
+    inf = number;
+    sup = 10 * number + 9;
+
+    function palindromikMi(n) {
+        var p = 0, q = n, r;
+        while (n > 0) {
+            r = n % 10;
+            p = 10 * p + r;
+            n = Math.floor(n / 10);
+        }
+        return p === q;
+    }
+
+    for (n = sup * sup, m = inf * inf; n > m; n -= 1) {
+        if (palindromikMi(n)) {
+            limit = Math.ceil(Math.sqrt(n));
+            d = sup;
+            while (d >= limit) {
+                if (n % d === 0 && n / d > inf) {
+                    return n;
+                }
+                d -= 1;
+            }
+        }
+    }
+    return NaN;
+}
+console.log(enBuyukPalindromik(3));
